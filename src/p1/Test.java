875fe69c8830d9ee671217f97c12713b852e66d6
VireshJ <jangamviresh@yahoo.com>
2020-08-08 00:23:07 +0530

    Resolved Conflicts

--- src/p1/Test.java
+++ src/p1/Test.java
@@ -5,5 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Branch A line 1");
 		System.out.println("Branch A line 2");
+		System.out.println("Branch B line 1");
+		System.out.println("Branch B line 2");
 	}
 }
